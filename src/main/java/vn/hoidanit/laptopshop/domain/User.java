f3ebcbbe636a7062a0e13c766ec453a590014725
vantu2004 <tulevan526@gmail.com>
2024-10-23 11:43:25 +0700

    completed chapter 10

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,21 +1,40 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="users")
 public class User {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	long id;
-	String email;
-	String password;
-	String fullName;
-	String address;
-	String phone;
+	private long id;
+	private String email;
+	private String password;
+	private String fullName;
+	private String address;
+	private String phone;
+	private String avatar;
+	
+	//	nhiều user thuộc 1 role, giữ khóa ngoại -> owner
+	@ManyToOne
+	//	nếu ko có annotation này thì bên mysql mặc định tự tạo 1 bảng trung gian, có annotation cho mysql biết 2 table liên kết theo role_id
+	//	role_id là tên tự đặt, thực tế nó liên kết tới thuộc tính đc đặt làm id (@Id) bên role
+	@JoinColumn(name = "role_id")
+	private Role role;
+	
+	//	1 user có nhiều order, ko giữ khóa ngoại -> mapped by order
+	@OneToMany(mappedBy = "user")
+	List<Order> orders;
 	
 	public User() {
 		
@@ -66,11 +85,17 @@ public class User {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-				+ ", address=" + address + ", phone=" + phone + "]";
+				+ ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
 	}
 
 }
